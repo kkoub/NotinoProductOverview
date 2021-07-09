3f0a527d97ebddcecadbd3162781be76e6bbee74
Jakub.Legindi <Jakub.Legindi@homecredit.cz>
2021-07-09 12:52:27 +0200

    removed unused code

--- presentation/src/test/java/cz/koubicl/notino/presentation/viewmodel/BaseTest.kt
+++ presentation/src/test/java/cz/koubicl/notino/presentation/viewmodel/BaseTest.kt
@@ -1,14 +1,11 @@
 package cz.koubicl.notino.presentation.viewmodel
 
-import io.reactivex.Scheduler
 import io.reactivex.android.plugins.RxAndroidPlugins
-import io.reactivex.functions.Function
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
 import org.junit.Before
 import org.koin.test.KoinTest
 import org.mockito.Mockito
-import java.util.concurrent.Callable
 
 open class BaseTest : KoinTest {
 
@@ -16,10 +13,6 @@ open class BaseTest : KoinTest {
     open fun setUp() {
         RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
         RxAndroidPlugins.setMainThreadSchedulerHandler { Schedulers.trampoline() }
-        RxJavaPlugins.setInitIoSchedulerHandler { Schedulers.trampoline() }
-        RxJavaPlugins.setInitComputationSchedulerHandler { Schedulers.trampoline() }
-        RxJavaPlugins.setInitNewThreadSchedulerHandler { Schedulers.trampoline() }
-        RxJavaPlugins.setInitSingleSchedulerHandler { Schedulers.trampoline() }
     }
 
     fun once() = Mockito.times(1)!!
